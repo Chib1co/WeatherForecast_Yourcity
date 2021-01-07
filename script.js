644f878fd61bea36c3cbed6c6e4bf0145063cb74
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-01-07 13:09:00 +1100

    commit to check

--- script.js
+++ script.js
@@ -1,15 +1,94 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-
+  generateCityList();
 
  function addingCities(){
     let citynames = $(".cityname");
     let cityList = JSON.parse(localStorage.getItem('city'))|| [];
-   
+ }//addingCities close
 
+ function resetDashboard(){
+   $(".city-info").text("");
+   $(".weather-forecast").text("");
+ }//resetDashboard close
 
- }
+ function createDiv(content){
+  const div  = document.createElement('div');
+  div.textContent = content;
+  return div
+ };//create Div close
+
+ function generateCityList() {
+   // grab local storage city entries
+   let cities = JSON.parse(window.localStorage.getItem("cities")) || [];
+
+   const ul = $('<ul>');
+
+   // loop thru cities, and create a list item for each of them
+   for (let index = 0; index < cities.length; index++) {
+     const city = cities[index];
+
+     const li = $('<li>');
+     li.text(city);
+     li.attr('class', 'cursor-pointer');
+     ul.append(li);
+   }
+
+   ul.on('click', function(event){
+     
+     const city = event.target.textContent;
+     searchWeather(city);
+
+   })
+   // put these li to the dom
+   $('.city-list').text('');
+   $('.city-list').append(ul);
+ }//generateCityList close
+
+ function searchWeather(cityName){
+  let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=6241153efb39f75fa72c3541aa78f172';
+  let cityCurrent = $("<div>");
+
+  // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+      console.log(response);
+
+  //let nameOfCity = $("<div>");
+  //nameOfCity.attr('class', 'nameOfCity');
+  let placeName = response.city.name;
+  //nameOfCity.text(placeName); 
+  cityCurrent.append(placeName);
+  //$(".city-info").append(nameOfCity);
+
+  let cityTemp = response.list[0].main.temp -273.15;
+  cityTempC = cityTemp.toFixed(2)
+  console.log(cityTempC);
+  cityCurrent.append(createDiv('Current Tempurature(C): ' + cityTempC));
+
+  let cityWind = response.list[0].wind.speed;
+  console.log(cityWind);
+  cityCurrent.append(createDiv('Current Wind :' + cityWind + ' km/h'));
+
+  let cityHumidity = response.list[0].main.humidity;
+  console.log(cityHumidity);
+  cityCurrent.append(createDiv('Current Humidity:' + cityHumidity + ' %'));
+
+  //   UX index
+  // let cityHumidity = response.list[0].main.humidity;
+  // console.log(cityHumidity);
+  // cityCurrent.append('Current Humidity' + cityHumidity + ' %');
+  resetDashboard();
+  $(".city-info").append(cityCurrent);
+
+  }); //ajax close
+
+ }//serchWeather close
+
 
 // .on("click") function associated with the Search Button
 $(".search-city").on("click", function(event) {
@@ -17,46 +96,24 @@ $(".search-city").on("click", function(event) {
     event.preventDefault();
     
     // let APIkey = '6241153efb39f75fa72c3541aa78f172';
-    let cityname = $(".cityname").val()
-    let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=6241153efb39f75fa72c3541aa78f172';
-    let cityCurrent = $("<div>");
-  
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response){
-        console.log(response);
+    let cityName = $(".cityname").val()
 
-    let cityTemp = response.list[0].main.temp -273.15;
-    cityTempC = cityTemp.toFixed(2)
-    console.log(cityTempC);
-    cityCurrent.append('Current Tempurature(C): ' + cityTempC);
+    searchWeather(cityName);
+    
 
-    let cityWind = response.list[0].wind.speed;
-    console.log(cityWind);
-    cityCurrent.append('Current Wind :' + cityWind + ' km/h');
+    let chosenCity = JSON.parse(window.localStorage.getItem("cities")) || [];
+    console.log(chosenCity)
+    let cityInput = $(".cityname").val();
+    chosenCity.unshift(cityInput);
 
-    let cityHumidity = response.list[0].main.humidity;
-    console.log(cityHumidity);
-    cityCurrent.append('Current Humidity:' + cityHumidity + ' %');
+    chosenCity = chosenCity.slice(0, 5);
 
-    //   UX index
-    // let cityHumidity = response.list[0].main.humidity;
-    // console.log(cityHumidity);
-    // cityCurrent.append('Current Humidity' + cityHumidity + ' %');
+    console.log(chosenCity)
 
-    $(".city-info").append(cityCurrent);
-
-    }); //ajax close
-
-    let chosenCity = window.localStorage.getItem("cities") || [];
-    let cityList = $(".cityname").val;
-
-    localStorage.setItem(JSON.stringify(chosenCity, cityList));
+    localStorage.setItem("cities", JSON.stringify(chosenCity));
 
 
+    generateCityList()
 
 
   }); //onclick close
